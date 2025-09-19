509b115a1134f3eebacdfc3a65ae703f36ab87c0
Logan Lambert (1000123473) <LAMBLK1@student.op.ac.nz>
2025-09-19 14:20:24 +1200

    added button to toggle between light and dark mode

--- WeatherApp/src/routes/+page.svelte
+++ WeatherApp/src/routes/+page.svelte
@@ -4,3 +4,12 @@
     <a href="/24hour">24-Hours</a>
     <a href="/7days">7 days</a>
 </nav>
+
+<button on:click={myFunction}>Toggle dark mode</button>
+
+<script>
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+</script>
